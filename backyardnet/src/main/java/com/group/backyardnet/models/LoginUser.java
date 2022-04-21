e7261a7d4e9f69653ca06eb4c6dde0dc998ad24b
ddarius42 <dariuszirbo@gmail.com>
2022-04-20 21:10:07 -0700

    made model files

---
+++ backyardnet/src/main/java/com/group/backyardnet/models/LoginUser.java
@@ -0,0 +1,5 @@
+package com.group.backyardnet.models;
+
+public class LoginUser {
+    
+}
---
+++ backyardnet/src/main/java/com/group/backyardnet/models/User.java
@@ -0,0 +1,12 @@
+package com.group.backyardnet.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name="users")
+public class User {
+    
+}
