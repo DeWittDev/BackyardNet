4dbfaea9fe138fca951f26b2c19b6fcb7762fdf0
ddarius42 <dariuszirbo@gmail.com>
2022-04-22 12:26:40 -0700

    created model tables with getters and setters

--- backyardnet/src/main/java/com/group/backyardnet/models/Comment.java
+++ backyardnet/src/main/java/com/group/backyardnet/models/Comment.java
@@ -3,10 +3,100 @@
 package com.group.backyardnet.models;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
-@Table(name="comments")
+@Table(name = "comments")
 public class Comment {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    @Size(min = 3, max = 200, message = "Item Name must be between 3 and 200 characters")
+    private String itemName;
+
+    @NotEmpty
+    @Size(min = 5, max = 200, message = "Description must be between 5 and 200 characters")
+    private String description;
+
+    @NotEmpty
+    @Size(min = 3, max = 200, message = "Item Name must be between 3 and 200 characters")
+    private String condition;
+
+    @NotEmpty
+    private Integer price;
+
+    @NotEmpty
+    @Size(min = 3, max = 200, message = "Item Name must be between 3 and 200 characters")
+    private Integer zipCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return this.itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCondition() {
+        return this.condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public Integer getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getZipCode() {
+        return this.zipCode;
+    }
+
+    public void setZipCode(Integer zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
 }
--- backyardnet/src/main/java/com/group/backyardnet/models/User.java
+++ backyardnet/src/main/java/com/group/backyardnet/models/User.java
@@ -2,13 +2,85 @@
 
 package com.group.backyardnet.models;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-
+import javax.persistence.Transient;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "This field cannot be left empty")
+    @Size(min = 3, max = 200, message = "Name must be between 3 and 200 characters")
+    private String name;
+
+    @NotEmpty(message = "This field cannot be left empty")
+    @Email(message = "Please enter a valid email address!")
+    private String email;
+
+    @NotEmpty(message = "This field cannot be left empty")
+    @Size(min = 8, max = 128, message = "Password must be between 8 and 128 characters")
+    private String password;
+
+    @Transient
+    @NotEmpty(message = "This field cannot be left empty")
+    @Size(min = 8, max = 128, message = "Password must be between 8 and 128 characters")
+    private String confirmPassword;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Item> item;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return this.confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public List<Item> getItem() {
+        return this.item;
+    }
+
+    public void setItem(List<Item> item) {
+        this.item = item;
+    }
 }
